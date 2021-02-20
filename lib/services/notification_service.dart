a6315c4779ecd205b7dd4804ea7c73fbc30b2412
eminakcil <eminakcil@outlook.com>
2021-02-20 19:23:15 +0300

    notification touch (document)

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 import 'package:crimyo/models/post.dart';
+import 'package:crimyo/screens/document/document_screen.dart';
 import 'package:crimyo/screens/post_detail/post_detail_screen.dart';
 import 'package:crimyo/services/navigation_service.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 
 class NotificationService {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
@@ -46,17 +49,35 @@ class NotificationService {
   void _serialiseAndNavigate(Map<String, dynamic> message) {
     var notificationData = message["data"];
     var view = notificationData["view"];
-    var postUrl = notificationData["post_url"];
-
+    var url = notificationData["url"];
 
     if (view != null) {
-      if (view == "post") {
-        NavigationService().navigatorKey.currentState.push(
-          MaterialPageRoute(
-            builder: (context) => PostDetailScreen(post: Post(url: postUrl)),
-          ),
-        );
+      var route;
+
+      switch (view) {
+        case "post":
+          route = MaterialPageRoute(
+            builder: (context) => PostDetailScreen(
+              post: Post(
+                url: url,
+              ),
+            ),
+          );
+          break;
+        case "document":
+          route = MaterialPageRoute(
+            builder: (context) => DocumentScreen(url: url),
+          );
+          break;
+        default:
+          break;
       }
+
+      if (route != null){
+        NavigationService().navigatorKey.currentState.push(route);
+      }
+
     }
   }
+
 }
